0c508b9461399a7a6b94e01c2c564e69800871bb
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2023-06-26 14:05:19 -0500

    Operacionescon Cadenas

---
+++ 18_cadenaCaracteres.py
@@ -0,0 +1,7 @@
+print('Hola \\ mundo')
+print('Hola \' mundo')
+print('Hola \n mundo')
+# Almacenando rutas
+print(r'c:\some\name')
+
+
---
+++ 19_OperacionesCadenas.py
@@ -0,0 +1,14 @@
+texto= "hola"
+print(texto *3 + " ultimo texto")
+
+# 
+cadena = 'python'
+
+print(cadena[0])
+print(cadena[3])
+print(cadena[:3])
+print(cadena[2:-2])
+
+print(cadena[2]=="t")
+
+print(len(cadena))
